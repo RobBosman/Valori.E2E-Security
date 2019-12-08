f54fbd85fde9e3326a15883ea38fe98ad4bcde08
Rob Bosman <rob.bosman.nl+github@gmail.com>
2019-12-08 12:59:00 +0100

    added local truststore for client certs

--- src/test/java/nl/valori/workshop/e2esecurity/HttpClientTest.java
+++ src/test/java/nl/valori/workshop/e2esecurity/HttpClientTest.java
@@ -1,5 +1,6 @@
 package nl.valori.workshop.e2esecurity;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,6 +10,13 @@ class HttpClientTest {
   private static final String URL_HTTP = "http://valori.example.com:80/";
   private static final String URL_HTTPS = "https://valori.example.com:443/";
   private static final String URL_HTTPS2 = "https://valori.example.com:2443/";
+
+  @BeforeEach
+  void setUp() {
+    System.setProperty("javax.net.ssl.trustStore", getClass().getResource("/truststore.jks").getPath());
+    System.setProperty("javax.net.ssl.trustStorePassword", "TruststorePassword");
+    System.setProperty("javax.net.ssl.trustStoreType", "JKS");
+  }
 
   @Test
   void connectHttpClient() {
